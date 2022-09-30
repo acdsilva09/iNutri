67a39f64fb68def46fd25e4f39896a9365c13b6c
Andre <acdsilva9@gmail.com>
2022-09-29 23:08:05 -0300

    Criação da Activity específica para o calculo TMB

--- app/src/main/java/com/example/inutri/MainActivity.java
+++ app/src/main/java/com/example/inutri/MainActivity.java
@@ -18,10 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     GoogleSignInClient googleSignInClient;
-    Button buttonCalcularTMB;
-    EditText editTextTextPeso, editTextTextIdade, editTextTextAltura;
-    RadioButton radioButtonSexoM, radioButtonSexoF;
-    TextView textViewResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,47 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        editTextTextAltura = (EditText)findViewById(R.id.editTextTextAltura);
-        editTextTextPeso = (EditText)findViewById(R.id.editTextTextPeso);
-        editTextTextIdade = (EditText)findViewById(R.id.editTextTextIdade);
-        radioButtonSexoF = (RadioButton) findViewById(R.id.radioButtonSexoF);
-        radioButtonSexoM = (RadioButton) findViewById(R.id.radioButtonSexoM);
-        buttonCalcularTMB = (Button) findViewById(R.id.buttonCalcularTMB);
-        textViewResultado = (TextView) findViewById(R.id.textViewResultado);
+
     }
 
 
-    public void clickButtonCalcular(View view){
-        if(editTextTextIdade.getText().length()>0
-                && editTextTextPeso.getText().length()>0
-                && editTextTextAltura.getText().length()>0){
-            if(radioButtonSexoF.isChecked()){
-                calculaTMBFeminino();
-            } else if(radioButtonSexoM.isChecked()){
-                calculaTMBMasculino();
-            }
 
-        }
-    }
 
-    private void calculaTMBFeminino() {
-        int altura = Integer.parseInt(String.valueOf(editTextTextAltura.getText()));
-        double peso = Double.parseDouble(String.valueOf(editTextTextPeso.getText()));
-        int idade = Integer.parseInt(String.valueOf(editTextTextIdade.getText()));
-
-        double resultado = 665 + 9.6 * peso + 1.8 * altura - 4.7 * idade;
-        textViewResultado.setText(String.valueOf(resultado));
-    }
-
-    private void calculaTMBMasculino() {
-        int altura = Integer.parseInt(String.valueOf(editTextTextAltura.getText()));
-        double peso = Double.parseDouble(String.valueOf(editTextTextPeso.getText()));
-        int idade = Integer.parseInt(String.valueOf(editTextTextIdade.getText()));
-
-        double resultado = 66 + 13.7 * peso + 5 * altura - 6.8 * idade;
-
-        textViewResultado.setText(String.valueOf(resultado));
-    }
 
 
 }
