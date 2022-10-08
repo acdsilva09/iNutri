82aa4337fa8e29cf38eb68dcf3b5b2eec6d1a8ec
Andre <acdsilva9@gmail.com>
2022-10-08 15:00:43 -0300

    Connect Firebase

--- app/src/main/java/com/example/inutri/MainActivity.java
+++ app/src/main/java/com/example/inutri/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.inutri;
 
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.inutri.databinding.ActivityMainBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ActivityMainBinding binding;
     GoogleSignInClient googleSignInClient;
+    //Button buttonLogin, buttonGoogleLogin;
+    //EditText editTextUser, editTextPassword;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -25,16 +38,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //buttonGoogleLogin = (Button) findViewById(R.id.buttonGoogleLogin);
+        //buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        //editTextUser = (EditText) findViewById(R.id.editTextUser);
+        //editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
 
         View view = binding.getRoot();
 
         setContentView(view);
 
+        mAuth = FirebaseAuth.getInstance();
 
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickButtonLogin(binding.editTextUser.getText().toString(),
+                                binding.editTextPassword.getText().toString());
+
+            }
+        });
 
 
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void clickButtonLogin(String email, String password ){
+
+        System.out.println("____________"+email+"________"+password);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCustomToken:success");
+
+                            Toast.makeText(getApplicationContext(), "Login Efetuado com Sucesso!!.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+    }
 
 
 
