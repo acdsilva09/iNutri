d821ee91e26d328ae0ac553a191430719a340ec3
André Dias <acdsilva9@gmail.com>
2024-01-01 17:37:00 -0300

    Sanner Camera

--- app/src/main/java/com/example/inutri/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/inutri/ui/camera/CameraFragment.java
@@ -1,49 +1,160 @@
 package com.example.inutri.ui.camera;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.camera.core.imagecapture.*;
+import com.example.inutri.databinding.FragmentCameraBinding;
+import com.google.common.util.concurrent.ListenableFuture;
 
-import com.example.inutri.R;
-import com.example.inutri.databinding.FragmentCameraBinding;
-import com.example.inutri.databinding.FragmentCameraBinding;
-import com.example.inutri.ui.camera.CameraViewModel;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+
 
 
 public class CameraFragment extends Fragment {
 
     private FragmentCameraBinding binding;
+    String currentPhotoPath;
+
+    private Bitmap imageBitmap = null;
+
+    PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private RecyclerView resultRv;
+
+    private ImageCapture imageCapture;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CameraViewModel galleryViewModel =
+        CameraViewModel cameraViewModel =
                 new ViewModelProvider(this).get(CameraViewModel.class);
 
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        
+        binding.scannerButton.setOnClickListener(v -> scannerButton());
+        binding.galeriaButton.setOnClickListener(v -> buscarGaleriaButton());
 
-        ImageCapture imageCapture =
-                new ImageCapture.Builder()
-                        .setTargetRotation(root.getDisplay().getRotation())
-                        .build();
+      //  ImageCapture imageCapture =
+      //          new ImageCapture.Builder()
+      //                  .setTargetRotation(root.getDisplay().getRotation())
+      //                  .build();
 
-        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, imageCapture, imageAnalysis, preview);
+
 
 
         return root;
     }
+
+    private void buscarGaleriaButton() {
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File f = new File(currentPhotoPath);
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void scannerButton() {
+// inside this method we are calling an implicit intent to capture an image.
+        imageBitmap =null;
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(getActivity(), "Erro ao capturar imagem: " + ex.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.example.inutri.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (imageBitmap != null) {
+                binding.imageCaptura.setImageBitmap(imageBitmap);
+            } else {
+                // Carregar a imagem do arquivo especificado pelo caminho
+                if (currentPhotoPath != null) {
+                    File imgFile = new File(currentPhotoPath);
+                    if (imgFile.exists()) {
+                        imageBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        binding.imageCaptura.setImageBitmap(imageBitmap);
+                    }
+                }
+            }
+        }
+    }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this.getActivity());
+    }
+
+
+
+
 
     @Override
     public void onDestroyView() {
